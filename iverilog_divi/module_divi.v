223903280a821ead4f5429b028ef3e80120b1c92
Edyson-Vargas <edvargasca@unal.edu.co>
2026-06-08 19:24:37 -0500

    module_divi.v

--- iverilog_divi/module_divi.v
+++ iverilog_divi/module_divi.v
@@ -18,7 +18,6 @@ wire Z_B;
 wire [15:0] A_reg;
 wire [15:0] B_reg;
 wire [15:0] SUB_RESULT;
-
 regA RA(.clk(clk), .rst(rst), .LD(LD), .SH(SH), .SUB(SUB), .A_in(A), 
   .SUB_RESULT(SUB_RESULT), .A_out(A_reg));
 regB RB(.clk(clk), .rst(rst), .LD(LD), .B_in(B), .B_out(B_reg));
@@ -26,7 +25,7 @@ comp_sub CS(.A(A_reg), .B(B_reg),.Z_A(Z_A), .SUB_RESULT(SUB_RESULT));
 regR RR(.clk(clk), .rst(rst), .LD(LD), .SH(SH), .BIT_IN(Z_A), .R(R));
 counter CNT(.clk(clk), .rst(rst), .ADD_C(ADD_C), .Z_B(Z_B));
 
-control CTRL(.clk(clk), .rst(rst), .init(init), .Z_A(Z_A), .Z_B(Z_B), .LD(LD), .SH(SH),
+control_divi CTRL(.clk(clk), .rst(rst), .init(init), .Z_A(Z_A), .Z_B(Z_B), .LD(LD), .SH(SH),
   .SUB(SUB), .ADD_C(ADD_C), .done(done));
 
 endmodule
